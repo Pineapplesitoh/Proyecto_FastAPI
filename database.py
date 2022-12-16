727a232b5076c7810658da7c939d723d931641fd
Jose Farias <jfariasnavea@gmail.com>
2022-12-16 13:51:32 -0300

    Modificando tabla de bd

--- database.py
+++ database.py
@@ -3,5 +3,17 @@ from peewee import *
 database = MySQLDatabase(
     'fastapi',
     user='root',password='Xcarpwesdiwem1',
-    host='localhost', port='3306' 
+    host='localhost', port= 3306 
 )
+
+class User(Model):
+    
+    username = CharField(max_length=50)
+    email = CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        database = database
+        table_name = 'users'
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
-from database import MySQLDatabase
+from database import database as connection
+from database import User
 
 app = FastAPI(title='Proyecto_FastAPI',
               description='Proyecto realizado en python',
               version='1.0.1')
+
+@app.on_event('startup')
+def startup():
+    if connection.is_closed():
+        connection.connect()
+        
+    connection.create_tables([User])
+
+@app.on_event('shutdown')
+def shutdown():
+    if not connection.is_closed():
+        connection.close()
 
 @app.get('/')
 async def index():
